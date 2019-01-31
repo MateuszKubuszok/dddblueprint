9fe65b2c5bd6140d421b6a5c07e532bd04df446f
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2019-01-31 11:41:55 +0100

    Made one action implementaion (kind of) complete

--- modules/common/src/main/scala/dddblueprint/compiler/ActionCompiler.scala
+++ modules/common/src/main/scala/dddblueprint/compiler/ActionCompiler.scala
@@ -2,11 +2,24 @@ package dddblueprint
 package compiler
 
 import cats.implicits._
-import cats.Monad
+import cats.{ Monad, Traverse }
 
-import scala.collection.immutable.ListSet
+import scala.collection.immutable.{ ListMap, ListSet }
 
 class ActionCompiler[F[_]: Monad: SchemaErrorRaise: SnapshotState](snapshotOperations: SnapshotOperations[F]) {
+
+  private def definitionExists(ref: schema.DefinitionRef): F[validated.DefinitionRef] =
+    for {
+      internalRef <- snapshotOperations.translateDefinitionRef(ref)
+      definitionExists <- snapshotOperations.hasDefinition(internalRef)
+      _ <- if (definitionExists) SchemaError.definitionExists[F, Unit](domain = ref.domain.name, name = ref.name)
+      else ().pure[F]
+    } yield internalRef
+
+  private val mapData: schema.Data => F[validated.Data] = {
+    case p: schema.Data.Primitive => (PrimitivesCompiler(p): validated.Data).pure[F]
+    case _ => ??? // TODO: implement - assume it was already defined when used
+  }
 
   def apply(action: schema.Action): F[Unit] = action match {
     case schema.Action.CreateDefinition(definition)           => createDefinition(definition)
@@ -20,10 +33,7 @@ class ActionCompiler[F[_]: Monad: SchemaErrorRaise: SnapshotState](snapshotOpera
   val createDefinition: schema.Data.Definition => F[Unit] = {
     case schema.Data.Definition.Enum(ref, values, ttype) =>
       for {
-        internalRef <- snapshotOperations.translateDefinitionRef(ref)
-        definitionExists <- snapshotOperations.hasDefinition(internalRef)
-        _ <- if (definitionExists) SchemaError.definitionExists[F, Unit](domain = ref.domain.name, name = ref.name)
-        else ().pure[F]
+        internalRef <- definitionExists(ref)
         internalType = PrimitivesCompiler.enumerable(ttype)
         _ <- snapshotOperations.setDefinition(internalRef,
                                               validated.Data.Definition.Enum(
@@ -33,9 +43,92 @@ class ActionCompiler[F[_]: Monad: SchemaErrorRaise: SnapshotState](snapshotOpera
                                               ))
       } yield ()
 
-    // TODO: other types
-    case _ =>
-      ???
+    case schema.Data.Definition.Record.Tuple(ref, fields) =>
+      for {
+        internalRef <- definitionExists(ref)
+        internalFields <- Traverse[ListMap[String, ?]].sequence[F, validated.Data](fields.map {
+          case (k, v) => k -> mapData(v)
+        })
+        _ <- snapshotOperations.setDefinition(internalRef,
+                                              validated.Data.Definition.Record.Tuple(
+                                                ref    = internalRef,
+                                                fields = internalFields
+                                              ))
+      } yield ()
+
+    case schema.Data.Definition.Record.Entity(ref, fields) =>
+      for {
+        internalRef <- definitionExists(ref)
+        internalFields <- Traverse[ListMap[String, ?]].sequence[F, validated.Data](fields.map {
+          case (k, v) => k -> mapData(v)
+        })
+        _ <- snapshotOperations.setDefinition(internalRef,
+                                              validated.Data.Definition.Record.Entity(
+                                                ref    = internalRef,
+                                                fields = internalFields
+                                              ))
+      } yield ()
+
+    case schema.Data.Definition.Record.Value(ref, fields) =>
+      for {
+        internalRef <- definitionExists(ref)
+        internalFields <- Traverse[ListMap[String, ?]].sequence[F, validated.Data](fields.map {
+          case (k, v) => k -> mapData(v)
+        })
+        _ <- snapshotOperations.setDefinition(internalRef,
+                                              validated.Data.Definition.Record.Value(
+                                                ref    = internalRef,
+                                                fields = internalFields
+                                              ))
+      } yield ()
+
+    case schema.Data.Definition.Record.Event(ref, fields) =>
+      for {
+        internalRef <- definitionExists(ref)
+        internalFields <- Traverse[ListMap[String, ?]].sequence[F, validated.Data](fields.map {
+          case (k, v) => k -> mapData(v)
+        })
+        _ <- snapshotOperations.setDefinition(internalRef,
+                                              validated.Data.Definition.Record.Event(
+                                                ref    = internalRef,
+                                                fields = internalFields
+                                              ))
+      } yield ()
+
+    case schema.Data.Definition.Service(ref, input, output) =>
+      for {
+        internalRef <- definitionExists(ref)
+        internalInput <- Traverse[ListSet].sequence[F, validated.DefinitionRef](input.map(definitionExists))
+        internalOutput <- Traverse[ListSet].sequence[F, validated.DefinitionRef](output.map(definitionExists))
+        _ <- snapshotOperations.setDefinition(internalRef,
+                                              validated.Data.Definition.Service(
+                                                ref    = internalRef,
+                                                input  = internalInput,
+                                                output = internalOutput
+                                              ))
+      } yield ()
+
+    case schema.Data.Definition.Publisher(ref, events) =>
+      for {
+        internalRef <- definitionExists(ref)
+        internalEvents <- Traverse[ListSet].sequence[F, validated.DefinitionRef](events.map(definitionExists))
+        _ <- snapshotOperations.setDefinition(internalRef,
+                                              validated.Data.Definition.Publisher(
+                                                ref    = internalRef,
+                                                events = internalEvents
+                                              ))
+      } yield ()
+
+    case schema.Data.Definition.Subscriber(ref, events) =>
+      for {
+        internalRef <- definitionExists(ref)
+        internalEvents <- Traverse[ListSet].sequence[F, validated.DefinitionRef](events.map(definitionExists))
+        _ <- snapshotOperations.setDefinition(internalRef,
+                                              validated.Data.Definition.Subscriber(
+                                                ref    = internalRef,
+                                                events = internalEvents
+                                              ))
+      } yield ()
   }
 
   // TODO: check if definition exists (should)
--- modules/common/src/main/scala/dddblueprint/compiler/package.scala
+++ modules/common/src/main/scala/dddblueprint/compiler/package.scala
@@ -4,9 +4,6 @@ import cats.mtl.{ FunctorRaise, MonadState }
 
 package object compiler {
 
-  type BlueprintState[F[_]] = MonadState[F, validated.Blueprint]
-  object BlueprintState { def apply[F[_]: BlueprintState]: BlueprintState[F] = implicitly[BlueprintState[F]] }
-
   type SnapshotState[F[_]] = MonadState[F, validated.Snapshot]
   object SnapshotState { def apply[F[_]: SnapshotState]: SnapshotState[F] = implicitly[SnapshotState[F]] }
 
