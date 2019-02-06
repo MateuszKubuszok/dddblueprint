607c1c6f64d2924631cc42cb7e15e51f01827156
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2019-02-06 12:30:28 +0100

    CreateDefinition tests

--- modules/laws/src/test/scala/dddblueprint/compiler/ActonCompilerSpec.scala
+++ modules/laws/src/test/scala/dddblueprint/compiler/ActonCompilerSpec.scala
@@ -6,15 +6,58 @@ import org.specs2.specification.Scope
 
 class ActonCompilerSpec extends CompilerSpec {
 
-  "ActionCompiler" should {
+  "ActionCompiler on CreateDefinition" should {
 
-    "correctly compile CreateDefinition for enum" in new Fixture {
+    "correctly compile enum definition" in new Fixture {
       val definition       = input.Fixtures.Data.Definition.Enum1
       val createDefinition = input.Action.CreateDefinition(definition)
 
       new TestSnapshot(ActionCompiler(createDefinition))() {
         val internalRef = definitionRefIso.get(definition.ref)
         snapshot.definitions(internalRef) === output.Fixtures.Data.Definition.Enum1.lens(_.ref).set(internalRef)
+      }
+    }
+
+    // TODO: in future ensure tuples are only used as arguments
+    "correctly compile tuple definition" in new Fixture {
+      val definition       = input.Fixtures.Data.Definition.Record.Tuple1
+      val createDefinition = input.Action.CreateDefinition(definition)
+
+      new TestSnapshot(ActionCompiler(createDefinition))() {
+        val internalRef = definitionRefIso.get(definition.ref)
+        snapshot.definitions(internalRef) === output.Fixtures.Data.Definition.Record.Tuple1.lens(_.ref).set(internalRef)
+      }
+    }
+
+    "correctly compile entity definition" in new Fixture {
+      val definition       = input.Fixtures.Data.Definition.Record.Entity1
+      val createDefinition = input.Action.CreateDefinition(definition)
+
+      new TestSnapshot(ActionCompiler(createDefinition))() {
+        val internalRef = definitionRefIso.get(definition.ref)
+        snapshot.definitions(internalRef) === output.Fixtures.Data.Definition.Record.Entity1
+          .lens(_.ref)
+          .set(internalRef)
+      }
+    }
+
+    "correctly compile value definition" in new Fixture {
+      val definition       = input.Fixtures.Data.Definition.Record.Value1
+      val createDefinition = input.Action.CreateDefinition(definition)
+
+      new TestSnapshot(ActionCompiler(createDefinition))() {
+        val internalRef = definitionRefIso.get(definition.ref)
+        snapshot.definitions(internalRef) === output.Fixtures.Data.Definition.Record.Value1.lens(_.ref).set(internalRef)
+      }
+    }
+
+    "correctly compile event definition" in new Fixture {
+      val definition       = input.Fixtures.Data.Definition.Record.Event1
+      val createDefinition = input.Action.CreateDefinition(definition)
+
+      new TestSnapshot(ActionCompiler(createDefinition))() {
+        val internalRef = definitionRefIso.get(definition.ref)
+        snapshot.definitions(internalRef) === output.Fixtures.Data.Definition.Record.Event1.lens(_.ref).set(internalRef)
       }
     }
   }
