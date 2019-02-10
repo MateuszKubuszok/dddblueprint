c58642fd0c99ea72d02c2868a9e6959e9b563a4e
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2019-02-10 21:42:10 +0100

    Drafted required validation test

--- modules/laws/src/test/scala/dddblueprint/compiler/ValidateTransitionSpec.scala
+++ modules/laws/src/test/scala/dddblueprint/compiler/ValidateTransitionSpec.scala
@@ -1,9 +1,33 @@
 package dddblueprint
 package compiler
 
+import io.scalaland.pulp.Provider
+import org.specs2.specification.Scope
+
 class ValidateTransitionSpec extends CompilerSpec {
+
+  "ValidateTransition" should {
+
+    "check if removed definitions are not used (all definitions exist)" in new Fixture {}
+
+    "check if types matches between versions" in new Fixture {}
+
+    "calculate required migrations for changed enum definitions" in new Fixture {}
+
+    "calculate required migrations for changed record definitions" in new Fixture {}
+
+    "calculate required migrations for changed service definitions" in new Fixture {}
+
+    "calculate required migrations for changed publisher definitions" in new Fixture {}
+
+    "calculate required migrations for changed subscriber definitions" in new Fixture {}
+  }
 
   // TODO: tuple can only be used inside its own domain as argument
 
   // TODO: event can only be published by publisher from its own domain
+
+  private trait Fixture extends Scope {
+    val ValidateTransition = Provider.get[ValidateTransition[F]]
+  }
 }
