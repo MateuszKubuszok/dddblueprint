b73ca94afeeb15c754dc5c6b398e3610832bcc09
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2019-02-21 23:27:26 +0100

    Created noop logger

--- modules/core/src/main/scala/dddblueprint/debug/Logging.scala
+++ modules/core/src/main/scala/dddblueprint/debug/Logging.scala
@@ -1,6 +1,9 @@
 package dddblueprint
 package debug
 
+import cats.Applicative
+import cats.implicits._
+
 trait Logging[F[_]] {
 
   def trace(msg: String): F[Unit]
@@ -14,4 +17,13 @@ trait Logging[F[_]] {
 object Logging {
 
   @inline def apply[F[_]](implicit logging: Logging[F]): Logging[F] = logging
+
+  def noop[F[_]: Applicative]: Logging[F] = new Logging[F] {
+    def trace(msg: String): F[Unit] = ().pure[F]
+    def debug(msg: String): F[Unit] = ().pure[F]
+    def info(msg:  String): F[Unit] = ().pure[F]
+    def warn(msg:  String): F[Unit] = ().pure[F]
+    def error(msg: String): F[Unit] = ().pure[F]
+    def error(msg: String, ex: Throwable): F[Unit] = ().pure[F]
+  }
 }
