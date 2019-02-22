42d612f4fe3614c352522003dff6641c0b32c0f1
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2019-02-22 18:49:30 +0100

    Log error for tests for easier development (disable by default)

--- modules/tests/src/test/scala/dddblueprint/compiler/CompilerSpec.scala
+++ modules/tests/src/test/scala/dddblueprint/compiler/CompilerSpec.scala
@@ -1,10 +1,23 @@
 package dddblueprint
 package compiler
 
+import alleycats.Zero
+import cats.implicits._
+import debug.syntax._
+
 trait CompilerSpec extends FRunningSpec {
 
-  abstract class TestSnapshot[A](result: StateIO[A])(base: output.Snapshot = output.Snapshot()) extends RefIso {
-    private lazy val snapshotA = result.run(base).apply()
+  abstract class TestSnapshot[A](result: StateIO[A])(base: output.Snapshot = Zero[output.Snapshot].zero)
+      extends RefIso {
+    private lazy val snapshotA = result
+      .handleErrorWith {
+        case ex @ SchemaError.Wrapper(errors) =>
+          error"Parsing failed:\n$errors".withEx[StateIO](ex).flatMap(_ => ex.raiseError[StateIO, A]): StateIO[A]
+        case ex: Throwable =>
+          error"Parsing failed".withEx[StateIO](ex).flatMap(_ => ex.raiseError[StateIO, A]): StateIO[A]
+      }
+      .run(base)
+      .apply()
     def snapshot: output.Snapshot = snapshotA._1
     def a:        A               = snapshotA._2
   }
--- modules/tests/src/test/scala/dddblueprint/parser/ParserSpec.scala
+++ modules/tests/src/test/scala/dddblueprint/parser/ParserSpec.scala
@@ -7,11 +7,15 @@ import debug.syntax._
 trait ParserSpec extends FRunningSpec {
 
   abstract class TestParsing(result: IO[input.History]) {
-    lazy val history = result.onErrorHandleWith {
-      case ex @ SchemaError.Wrapper(errors) =>
-        error"Parsing failed:\n$errors".withEx[IO](ex).flatMap(_ => ex.raiseError[IO, input.History])
-      case ex: Throwable =>
-        error"Parsing failed".withEx[IO](ex).flatMap(_ => ex.raiseError[IO, input.History])
-    }()
+    lazy val history = result
+      .handleErrorWith {
+        case ex @ SchemaError.Wrapper(errors) =>
+          error"Parsing failed:\n$errors"
+            .withEx[IO](ex)
+            .flatMap(_ => ex.raiseError[IO, input.History]): IO[input.History]
+        case ex: Throwable =>
+          error"Parsing failed".withEx[IO](ex).flatMap(_ => ex.raiseError[IO, input.History]): IO[input.History]
+      }
+      .apply()
   }
 }
