1a4bbacc9f0b6319cc741abebdb0a0360f1d8678
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2020-05-29 23:36:43 +0200

    Small step forward towards generating output

--- modules/backend/src/main/scala/dddblueprint/backend/CatsBackend.scala
+++ modules/backend/src/main/scala/dddblueprint/backend/CatsBackend.scala
@@ -1,7 +1,7 @@
 package dddblueprint
 package backend
 
-import java.nio.file.Path
+import java.nio.file.{ Path, Paths }
 
 import cats.effect.Sync
 import cats.implicits._
@@ -16,41 +16,59 @@ final class CatsBackend[IO[_]: Sync: SchemaErrorRaise](pkg: String) extends Back
 
     def asPackageName: String = string.toLowerCase.replaceAll("[- ]", "_")
     def asClassName:   String = string.replaceAll("[- ]", "_")
+
+    // TODO: use better error handling
+    def parseIO[U: scala.meta.parsers.Parse]: IO[U] = Sync[IO].delay(string.parse[U].get)
   }
 
-  def apply(blueprint: Blueprint): IO[ListMap[Path, String]] = ???
+  def apply(blueprint: Blueprint): IO[ListMap[Path, String]] =
+    blueprint.versions
+      .flatTraverse { snapshot: Snapshot =>
+        /*
+         TODO:
+         1. foreach snapshot
+         2. - generate ADT for each type
+         3. - generate trait for each service
+         4. - generate migration
+         */
+        val versioned = getVersionedDefinitionBody(snapshot)
+        snapshot.definitions.toList.traverse {
+          case (_, definition) =>
+            versioned(definition).map(Paths.get("test") -> _.toString)
+        }
+      }
+      .map(l => ListMap(l.toSeq: _*))
 
   def getFullName(snapshot: Snapshot)(ref: DefinitionRef): IO[Type] =
     snapshot
       .findDomainNameAndName(ref)
       .map {
         case (domainName, name) =>
-          val version = snapshot.namespaces.versions(ref)
-          show"$pkg.${domainName.asPackageName}.${name.asClassName}.$version".parse[Type].get.pure[IO]
+          show"$pkg.${domainName.asPackageName}.${name.asClassName}.${snapshot.namespaces.versions(ref)}".parseIO[Type]
       }
       .getOrElse(SchemaError.invalidRef(ref.id))
 
   val getPrimitiveType: Data.Primitive => IO[Type] = Map(
-    Data.UUID -> "java.util.UUID".parse[Type].get.pure[IO],
-    Data.Boolean -> "scala.Boolean".parse[Type].get.pure[IO],
-    Data.Int -> "scala.Int".parse[Type].get.pure[IO],
-    Data.Long -> "scala.Long".parse[Type].get.pure[IO],
-    Data.Float -> "scala.Float".parse[Type].get.pure[IO],
-    Data.Double -> "scala.Double".parse[Type].get.pure[IO],
-    Data.String -> "scala.Predef.String".parse[Type].get.pure[IO]
+    Data.UUID -> "java.util.UUID".parseIO[Type],
+    Data.Boolean -> "scala.Boolean".parseIO[Type],
+    Data.Int -> "scala.Int".parseIO[Type],
+    Data.Long -> "scala.Long".parseIO[Type],
+    Data.Float -> "scala.Float".parseIO[Type],
+    Data.Double -> "scala.Double".parseIO[Type],
+    Data.String -> "scala.Predef.String".parseIO[Type]
   )
 
   def getCollectionType(snapshot: Snapshot): Data.Collection => IO[Type] = {
     val argument: Argument => IO[Type] = getArgumentType(snapshot)
     ({
-      case Data.Collection.Option(of) => argument(of).map(tpe => show"scala.Option[${tpe.toString}]".parse[Type].get)
-      case Data.Collection.Array(of)  => argument(of).map(tpe => show"scala.List[${tpe.toString}]".parse[Type].get)
+      case Data.Collection.Option(of) => argument(of).flatMap(tpe => show"scala.Option[${tpe.toString}]".parseIO[Type])
+      case Data.Collection.Array(of)  => argument(of).flatMap(tpe => show"scala.List[${tpe.toString}]".parseIO[Type])
       case Data.Collection.Set(of) =>
-        argument(of).map(tpe => show"scala.Predef.Set[${tpe.toString}]".parse[Type].get)
+        argument(of).flatMap(tpe => show"scala.Predef.Set[${tpe.toString}]".parseIO[Type])
       case Data.Collection.Map(k, v) =>
-        (argument(k) -> argument(v)).mapN((ktpe, vtpe) =>
-          show"scala.Predef.Map[${ktpe.toString}, ${vtpe.toString}]".parse[Type].get
-        )
+        (argument(k) -> argument(v))
+          .mapN((ktpe, vtpe) => show"scala.Predef.Map[${ktpe.toString}, ${vtpe.toString}]".parseIO[Type])
+          .flatten
     }: Data.Collection => IO[Type])
   }
 
@@ -58,7 +76,7 @@ final class CatsBackend[IO[_]: Sync: SchemaErrorRaise](pkg: String) extends Back
     val argument: Argument => IO[Type] = getArgumentType(snapshot)
     ({
       case Data.Tuple(args) =>
-        Traverse[List].sequence(args.map(argument)).map(types => show"(${types.mkString(",")})".parse[Type].get)
+        Traverse[List].sequence(args.map(argument)).flatMap(types => show"(${types.mkString(",")})".parseIO[Type])
     }: Data.Tuple => IO[Type])
   }
 
@@ -69,25 +87,32 @@ final class CatsBackend[IO[_]: Sync: SchemaErrorRaise](pkg: String) extends Back
     case t: Data.Tuple      => getTupleType(snapshot)(t)
   }
 
-  // TODO
-  def getVersionedDefinitionBody(snapshot: Snapshot): Data.Definition => IO[Source] = { _ =>
-    snapshot.hashCode()
-    "{}".parse[Source].get.pure[IO]
-  }
-  /*
   def getVersionedDefinitionBody(snapshot: Snapshot): Data.Definition => IO[Source] = {
-    case Data.Definition.Record.Entity(ref, fields)  => "".parse[Source].get.pure[IO]
-    case Data.Definition.Record.Value(ref, fields)   => "".parse[Source].get.pure[IO]
-    case Data.Definition.Record.Event(ref, fields)   => "".parse[Source].get.pure[IO]
-    case Data.Definition.Service(ref, input, output) => "".parse[Source].get.pure[IO]
-    case Data.Definition.Publisher(ref, events)      => "".parse[Source].get.pure[IO]
-    case Data.Definition.Subscriber(ref, events)     => "".parse[Source].get.pure[IO]
+    val toType: Argument => IO[Type] = getArgumentType(snapshot)
+    def ver(ref:     DefinitionRef) = snapshot.namespaces.versions(ref)
+    def args(fields: Data.Definition.FieldSet) =
+      fields.toList
+        .traverse { case (name, arg) => toType(arg).map(tpe => show"""$name: ${tpe.toString}""") }
+        .map(_.mkString(", "))
+    def out(output: Data.Definition.RefSet) =
+      output.toList.traverse(out => toType(out).map(tpe => show"""${tpe.toString}""")).map(_.mkString(", "))
+    // TODO: add also ver and type  to output type if needed!!!!
+    ({
+      case Data.Definition.Record.Entity(ref, fields) =>
+        // TODO: add ID
+        args(fields).flatMap(a => show"""final case class ${ver(ref)}($a)""".parseIO[Source])
+      case Data.Definition.Record.Value(ref, fields) =>
+        args(fields).flatMap(a => show"""final case class ${ver(ref)}($a)""".parseIO[Source])
+      case Data.Definition.Record.Event(ref, fields) =>
+        args(fields).flatMap(a => show"""final case class ${ver(ref)}($a)""".parseIO[Source])
+      case Data.Definition.Service(ref, input, output) =>
+        (args(input), out(output)).tupled.flatMap { case (a, o) => show"""def ${ver(ref)}($a): ($o)""".parseIO[Source] }
+      case Data.Definition.Publisher(ref, events) =>
+        out(events).flatMap(o => show"""def ${ver(ref)}: Publisher[($o)]""".parseIO[Source])
+      case Data.Definition.Subscriber(ref, events) =>
+        out(events).flatMap(o => show"""def ${ver(ref)}: Subscriber[($o)]""".parseIO[Source])
+    }: Data.Definition => IO[Source])
   }
-   */
-
-  // TODO: rename handling?!?! (actually it can be done with getFullName)
-
-  // TODO: create automatic migrations that do not require manual intervention
 }
 
 object CatsBackend {}
